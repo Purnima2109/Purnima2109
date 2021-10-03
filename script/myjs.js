d408676b087e4e55a4ccdd55a12dadaa32c1622d
groverpurnima2109 <groverpurnima2109@gmail.com>
2021-10-03 23:41:06 +0530

    Changed code for responsiveness

--- script/myjs.js
+++ script/myjs.js
@@ -23,18 +23,22 @@ function page4() {
     document.getElementById("experience").style.display = "none";
     document.getElementById("home").style.display = "none";
 }
-var date = new Date()
-var time = date.getHours()
-var str = "";
-if (time < 12) {
-    str = "Good Morning!!!";
-} else if (time < 18) {
-    str = "Good Afternoon!!!";
-} else {
-    str = "Good Evening!!!";
-}
-let month=date.getMonth()+1;
-document.getElementById("greet").innerHTML = str;
-document.getElementById("date").innerHTML = "Today is "+date.getDate()+"/"+month+"/"+date.getFullYear();
-document.getElementById("time").innerHTML = "Current time is "+ date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
 
+
+  setInterval(function(){
+    var date = new Date()
+    var time = date.getHours()
+    var str = "";
+    if (time < 12) {
+        str = "Good Morning!!!";
+    } else if (time < 18) {
+        str = "Good Afternoon!!!";
+    } else {
+        str = "Good Evening!!!";
+    }
+    let month=date.getMonth()+1;
+    document.getElementById("greet").innerHTML = str;
+    document.getElementById("date").innerHTML = "Today is "+date.getDate()+"/"+month+"/"+date.getFullYear();
+    document.getElementById("time").innerHTML = "Current time is "+ date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+    
+  }, 1000);
